55188b52ffe9ced76f4b0c27c7872780c50881c8
madao <madao@qq.com>
2023-09-10 21:13:16 +0800

    懒汉式静态内部类单例实现

--- src/main/java/com/madao/designpattern/singletonpattern/StaticInnerClass.java
+++ src/main/java/com/madao/designpattern/singletonpattern/StaticInnerClass.java
@@ -1,4 +1,17 @@
 package com.madao.designpattern.singletonpattern;
 
 public class StaticInnerClass {
+
+
+    private StaticInnerClass() {
+
+    }
+
+    public StaticInnerClass getInstance() {
+        return SingletonInstanceHolder.instance;
+    }
+
+    private static class SingletonInstanceHolder {
+        private static final StaticInnerClass instance = new StaticInnerClass();
+    }
 }
